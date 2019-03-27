ce42c5a4ebccf0c1a6a9520b9f002b374f23f632
wchampionjohn <vhitotoyouv@gmail.com>
2019-03-27 22:29:12 +0800

    db column rename name

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,6 +1,6 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
-  belongs_to :product, class_name: 'Product::Spec', foreign_key: 'product_spec_id'
+  belongs_to :product, class_name: 'Product::Spec', foreign_key: 'product_id'
 
   def price
     product.price * quantity
